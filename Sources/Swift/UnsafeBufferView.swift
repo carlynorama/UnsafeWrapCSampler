603706205b3d8643a75f92429ecb60afb4c9a742
carlynorama <carlynorama@gmail.com>
2023-03-29 10:00:54 -0700

    add last code snippet from video

---
+++ Sources/Swift/UnsafeBufferView.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  
+//
+//  Created by Carlyn Maw on 3/29/23.
+//  Code from 25:52 of WWDC 2020 "Safely Manage Pointers in Swift."
+
+// In the subscript, using .load(fromByteOffset:as) prevents rebinding of memory for access. This struct can point to memory bound as a different type without overriding that.
+
+import Foundation
+
+struct UnsafeBufferView<Element>: RandomAccessCollection {
+    
+    let rawBytes: UnsafeRawBufferPointer
+    let count: Int
+    
+    init(reinterpret rawBytes:UnsafeRawBufferPointer, as: Element.Type) {
+        self.rawBytes = rawBytes
+        self.count = rawBytes.count / MemoryLayout<Element>.stride
+        precondition(self.count * MemoryLayout<Element>.stride == rawBytes.count)
+        precondition(Int(bitPattern: rawBytes.baseAddress).isMultiple(of: MemoryLayout<Element>.alignment))
+    }
+    
+    public var startIndex: Int { 0 }
+    public var endIndex: Int { count }
+    
+    subscript(position: Int) -> Element {
+        rawBytes.load(fromByteOffset: position * MemoryLayout<Element>.stride, as: Element.self)
+    }
+    
+}
+

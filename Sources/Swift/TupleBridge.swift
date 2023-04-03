4a5ce72fb9fc92d038ef094fbb29c6d3498b1178
carlynorama <carlynorama@gmail.com>
2023-04-02 18:20:50 -0700

    don't. get. distracted.

--- Sources/Swift/TupleBridge.swift
+++ Sources/Swift/TupleBridge.swift
@@ -23,27 +23,20 @@ fileprivate func tupleEraser() {
 }
 
 
+
 // TupleBridge is experimental. It all "works", but mostly for use as reference.
 
 public struct TupleBridge<N:Numeric> {
     let size:Int
     let values:[N]
     
-    /// usage:
-    ///    `public func fetchBaseBufferRGBA() -> [UInt32] {
-    ///    `    fetchFixedSizeCArray(source: random_provider_RGBA_array, boundToType: UInt32.self)
-    ///    `}
-    static func fetchFixedSizeCArray<T, R>(source:T, boundToType:R.Type) -> [R] {
-        withUnsafeBytes(of: source) { (rawPointer) -> [R] in
-            let bufferPointer = rawPointer.assumingMemoryBound(to: boundToType)
-            return [R](bufferPointer)
-        }
-    }
+
     
     public init(array:[N]) {
         self.values = array
         self.size = values.count
     }
+    
     
     public init<U>(tuple: U, count:Int, type:N.Type) {
         precondition(MemoryLayout.size(ofValue: tuple) == MemoryLayout<N>.stride * count)
@@ -111,6 +104,66 @@ extension TupleBridge where N == CInt {
             
         }
     }
+}
+
+
+
+//DISTRACTION: Get some insights into passing a Generic into a Generic
+//Works, here outside of TupleBridge, but have to specify type on TupleBridge?
+//Has nothing to do with type of return. That is the parameter.
+//func genericToGenericTest() {
+//    let array:[UInt32] = TupleBridge<UInt16>.fetchFixedSizeCArray(source:random_provider_RGBA_array, boundToType:UInt32.self)
+//    let tmp = TupleBridge(array: array)
+//}
+
+//This one does not need to specify.
+//func genericToGenericTest2() {
+//    let array = MiscHandy().fetchFixedSizeCArray(source:random_provider_RGBA_array, boundToType:UInt32.self)
+//    let tmp = TupleBridge(array: array)
+//}
+
+extension TupleBridge {
+    
+    //This static function will keep you from getting the tuple as a tuple.
+    /// usage:
+    ///    `public func fetchBaseBufferRGBA() -> [UInt32] {
+    ///    `    fetchFixedSizeCArray(source: random_provider_RGBA_array, boundToType: UInt32.self)
+    ///    `}
+//    static func fetchFixedSizeCArray<T, R>(source:T, boundToType:R.Type) -> [R] {
+//        withUnsafeBytes(of: source) { (rawPointer) -> [R] in
+//            let bufferPointer = rawPointer.assumingMemoryBound(to: boundToType)
+//            return [R](bufferPointer)
+//        }
+//    }
+    
+    //Cannot convert value of type '[UInt32]' to expected argument type '[N]'
+//    static func TupleBridgeMaker() -> TupleBridge {
+//        let array  = Self.fetchFixedSizeCArray(source: random_provider_RGBA_array, boundToType: UInt32.self)
+//        let tmp = Self.init(array: array)
+//        return tmp
+//    }
+    //
+//    public init<T, N>(source:T, boundToType type:N.Type){
+//        self.init(array: Self.fetchFixedSizeCArray(source: source, boundToType: N.self))
+//    }
+//
+//    public init<T, N>(src:T, boundToType type:N.Type){
+//        self.init(array: MiscHandy().fetchFixedSizeCArray(source: src, boundToType: type))
+//    }
+//
+//    public init<T, N>(source:T, bindTo type:N.Type){
+//        let tmp = MiscHandy().fetchFixedSizeCArray(source: source, boundToType: type)
+//        self.init(array: tmp)
+//    }
+    
+    //Cannot convert value of type '(UnsafeRawBufferPointer) -> [N]' to expected argument type '(UnsafeRawBufferPointer) throws -> [N]'
+//    public init<T, N>(source:T, boundToType:N.Type) {
+//        self.values = withUnsafeBytes(of: source) { (rawPointer) -> [N] in
+//            let bufferPointer = rawPointer.assumingMemoryBound(to: boundToType)
+//            return [N](bufferPointer)
+//        }
+//        self.size = values.count
+//    }
 }
 
 
